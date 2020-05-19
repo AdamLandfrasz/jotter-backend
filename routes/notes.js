03676b522b4374d5e192c47e1139ec0f76c84a49
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-05-19 11:19:10 +0200

    implemented get notes api endpoint

--- routes/notes.js
+++ routes/notes.js
@@ -5,4 +5,13 @@ const authRequired = require("../middleware/authRequired");
 
 const Note = require("../models/Note");
 
+router.get("/", authRequired, async (req, res) => {
+  try {
+    const userNotes = await Note.find({ owner: req.userId });
+    res.status(200).json({ success: true, userNotes });
+  } catch (e) {
+    res.status(400).json({ success: false, message: e.toString() });
+  }
+});
+
 module.exports = router;

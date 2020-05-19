03986f70d5cda68addc5c83b1166ea7718f80777
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-05-19 11:35:55 +0200

    created empty endpoints for note edit and delete

--- routes/notes.js
+++ routes/notes.js
@@ -8,13 +8,11 @@ const Note = require("../models/Note");
 router.get("/", authRequired, async (req, res) => {
   try {
     const userNotes = await Note.find({ owner: req.userId });
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Notes returned successfully.",
-        userNotes,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Notes returned successfully.",
+      userNotes,
+    });
   } catch (e) {
     res.status(400).json({ success: false, message: e.toString() });
   }
@@ -30,16 +28,18 @@ router.post("/", authRequired, async (req, res) => {
       noteType: req.body.type || undefined,
     });
     const savedNote = await newNote.save();
-    res
-      .status(201)
-      .json({
-        success: true,
-        message: "Note created successfully.",
-        savedNote,
-      });
+    res.status(201).json({
+      success: true,
+      message: "Note created successfully.",
+      savedNote,
+    });
   } catch (e) {
     res.status(400).json({ success: false, message: e.toString() });
   }
 });
+
+router.put("/", authRequired, (req, res) => {});
+
+router.delete("/", authRequired, (req, res) => {});
 
 module.exports = router;

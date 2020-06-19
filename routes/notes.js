54d2fc9176daec889ff47f78ab9e80322c6ae0f1
Adam Landfrasz <adam.landfrasz@gmail.com>
2020-06-19 14:23:01 +0200

    added share note endpoint

--- routes/notes.js
+++ routes/notes.js
@@ -4,14 +4,17 @@ const router = express.Router();
 const authRequired = require("../middleware/authRequired");
 
 const Note = require("../models/Note");
+const User = require("../models/User");
 
 router.get("/", authRequired, async (req, res) => {
   try {
     const userNotes = await Note.find({ owner: req.userId });
+    const sharedNotes = await Note.find({ sharedWith: req.userId });
+    const returnedNotes = userNotes.concat(sharedNotes);
     res.status(200).json({
       success: true,
       message: "Notes returned successfully.",
-      userNotes,
+      userNotes: returnedNotes,
     });
   } catch (e) {
     res.status(400).json({ success: false, message: e.toString() });
@@ -49,6 +52,27 @@ router.put("/", authRequired, async (req, res) => {
       success: true,
       message: "Note updated successfully.",
       editedNote,
+    });
+  } catch (e) {
+    res.status(400).json({ success: false, message: e.toString() });
+  }
+});
+
+router.put("/share", authRequired, async (req, res) => {
+  try {
+    const userToShareWith = await User.findOne({ email: req.body.userEmail });
+    const noteToShare = await Note.findById(req.body.noteId);
+    if (!userToShareWith || !noteToShare) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User or Note not found!" });
+    }
+    await noteToShare.updateOne({
+      sharedWith: [...noteToShare.sharedWith, userToShareWith._id],
+    });
+    res.status(200).json({
+      success: true,
+      message: "Note shared successfully.",
     });
   } catch (e) {
     res.status(400).json({ success: false, message: e.toString() });
